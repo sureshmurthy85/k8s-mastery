7d4e974e910a97c4e2376f74351a9e7651cdd8c1
sureshmurthy85 <ssridharamurthy@cloudbees.com>
2020-08-06 17:23:17 -0700

    Changed the port to run sa-webapp and fixed CrossOrigin filter issue

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://localhost:9090/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
--- sa-webapp/src/main/java/com/sa/web/SentimentController.java
+++ sa-webapp/src/main/java/com/sa/web/SentimentController.java
@@ -2,28 +2,30 @@ package com.sa.web;
 
 import com.sa.web.dto.SentenceDto;
 import com.sa.web.dto.SentimentDto;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin
 @RestController
-public class SentimentController {
-
+public class SentimentController
+{
     @Value("${sa.logic.api.url}")
     private String saLogicApiUrl;
 
     @PostMapping("/sentiment")
-    public SentimentDto sentimentAnalysis(@RequestBody SentenceDto sentenceDto) {
+    public SentimentDto sentimentAnalysis(@RequestBody SentenceDto sentenceDto)
+    {
         RestTemplate restTemplate = new RestTemplate();
-
         return restTemplate.postForEntity(saLogicApiUrl + "/analyse/sentiment",
-                sentenceDto, SentimentDto.class)
-                .getBody();
+            sentenceDto, SentimentDto.class)
+                           .getBody();
     }
 
     @GetMapping("/testHealth")
-    public void testHealth() {
+    public void testHealth()
+    {
     }
 }
 
